1588fa8c26d53fdd30f90f3f34c4b9e98f677831
Dicot0721 <149964080+Dicot0721@users.noreply.github.com>
2025-11-23 00:53:55 +0800

    perf: Optimize Logger

--- include/ame_logger.h
+++ include/ame_logger.h
@@ -20,7 +20,6 @@
 #ifndef AME_LOGGER_H
 #define AME_LOGGER_H
 
-#include <array>
 #include <chrono>
 #include <format>
 #include <iostream>
@@ -33,20 +32,20 @@ enum class LogLevel {
     INFO,
     WARN,
     ERROR,
-    OFF,
+    SILENT,
 };
 
 class Logger {
 public:
-    [[nodiscard, gnu::visibility("default")]] static Logger &Instance() {
-        static Logger logger;
-        return logger;
-    }
-
     Logger(const Logger &) = delete;
     Logger(Logger &&) = delete;
     Logger &operator=(const Logger &) = delete;
     Logger &operator=(Logger &&) = delete;
+
+    [[nodiscard, gnu::visibility("default")]] static Logger &Instance() {
+        static Logger logger;
+        return logger;
+    }
 
     [[nodiscard]] LogLevel GetLevel() const noexcept {
         return _level;
@@ -57,59 +56,40 @@ public:
     }
 
     template <typename... Args>
-    void Debug(std::source_location location, std::format_string<Args...> format, Args &&...args) {
-        Output(location, LogLevel::DEBUG, format.get(), std::forward<Args>(args)...);
-    }
-
-    template <typename... Args>
-    void Info(std::source_location location, std::format_string<Args...> format, Args &&...args) {
-        Output(location, LogLevel::INFO, format.get(), std::forward<Args>(args)...);
-    }
-
-    template <typename... Args>
-    void Warn(std::source_location location, std::format_string<Args...> format, Args &&...args) {
-        Output(location, LogLevel::WARN, format.get(), std::forward<Args>(args)...);
-    }
-
-    template <typename... Args>
-    void Error(std::source_location location, std::format_string<Args...> format, Args &&...args) {
-        Output(location, LogLevel::ERROR, format.get(), std::forward<Args>(args)...);
-    }
-
-protected:
-    Logger() = default;
-    ~Logger() = default;
-
-    void Output(std::source_location location, LogLevel level, std::string_view format, auto &&...args) {
+    void Log(std::source_location location, LogLevel level, std::format_string<Args...> format, Args &&...args) {
         if (level < _level) {
             return;
         }
 
         std::ostream &stream = (level < LogLevel::ERROR) ? std::clog : std::cerr;
         const auto now = std::chrono::system_clock::now();
-        const std::string message = std::vformat(format, std::make_format_args(args...));
+        const std::string message = std::vformat(format.get(), std::make_format_args(args...));
 
         // "\033[39m" -> default color
-        std::println(stream, "{}[{:%T}][{}] [{}] {}\033[39m", _colorStrs[int(level)], now, _levelChars[int(level)], location.function_name(), message);
+        std::println(stream, "{}[{:%T}][{}] [{}] {}\033[39m", _colorStrs[int(level)], now, _levelCodes[int(level)], location.function_name(), message);
     }
 
-    static constexpr std::array<std::string_view, 4> _colorStrs = {
+protected:
+    Logger() = default;
+    ~Logger() = default;
+
+    static constexpr std::string_view _colorStrs[int(LogLevel::SILENT)] = {
         "\033[32m", // green
         "",
         "\033[33m", // yellow
         "\033[31m", // red
     };
 
-    static constexpr std::array<char, 4> _levelChars = {'D', 'I', 'W', 'E'};
+    static constexpr char _levelCodes[int(LogLevel::SILENT)] = {'D', 'I', 'W', 'E'};
 
     LogLevel _level = LogLevel::DEBUG;
 };
 
 } // namespace ame
 
-#define LOG_DEBUG(...) ame::Logger::Instance().Debug(std::source_location::current(), __VA_ARGS__)
-#define LOG_INFO(...) ame::Logger::Instance().Info(std::source_location::current(), __VA_ARGS__)
-#define LOG_WARN(...) ame::Logger::Instance().Warn(std::source_location::current(), __VA_ARGS__)
-#define LOG_ERROR(...) ame::Logger::Instance().Error(std::source_location::current(), __VA_ARGS__)
+#define LOG_DEBUG(...) ame::Logger::Instance().Log(std::source_location::current(), ame::LogLevel::DEBUG, __VA_ARGS__)
+#define LOG_INFO(...) ame::Logger::Instance().Log(std::source_location::current(), ame::LogLevel::INFO, __VA_ARGS__)
+#define LOG_WARN(...) ame::Logger::Instance().Log(std::source_location::current(), ame::LogLevel::WARN, __VA_ARGS__)
+#define LOG_ERROR(...) ame::Logger::Instance().Log(std::source_location::current(), ame::LogLevel::ERROR, __VA_ARGS__)
 
 #endif // AME_LOGGER_H
